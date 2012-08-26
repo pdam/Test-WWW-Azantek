d65dd06ff043fc96e4c0edac2bca08bc7b9572f5
Pratik Dam <pdam.2010@gmail.com>
2012-08-26 22:30:10 +0100

    Updated

---
+++ Build.pl
@@ -0,0 +1,202 @@
+#!/usr/bin/env perl
+use strict;
+use Module::Build;
+
+my $build = Module::Build->new(
+    dist_author          => 'Prtik Dam <pdam@gmail.com>',
+    module_name          => 'WWW::Azantek',
+    dist_version_from    => 'lib/WWW/Azantek.pm',
+    dist_abstract        => 'WWW::Azantek',
+    license              => 'perl',
+    recursive_test_files => 1,
+    create_readme        => 1,
+    configure_requires   => {},
+    create_makefile_pl   => 'traditional',
+    build_requires       => {
+        'Test::More'                => '0', 
+        'File::Slurp'               => '0',
+        'Test::Exception'           => '0',
+        'Test::MockObject::Extends' => '0',
+        'Test::Perl::Critic'        => '0',
+        'Test::Prereq::Build'       => '0',
+        'Env'                       => '0',
+    },
+    requires => {
+        'Module::Build'             => '0.30',
+        'Data::Dumper'           => '0',    #Debugging
+        'WWW::Mechanize'         => '0',
+        'WWW::Mechanize::Cached' => '0',
+        'Carp'                   => '0',
+    },
+    meta_merge => {
+        resources => {
+            homepage => 'http://www.azantek.com',
+            bugtracker =>
+                'http://Azantek.atlassian.net',
+            repository => 'http://github.com/pdam/Test-WWW-Azantek',
+        }
+    },
+
+);
+
+my $build2 = Module::Build->new(
+    dist_author          => 'Prtik Dam <pdam@gmail.com>',
+    module_name          => 'WWW::Azantek::Diary',
+    dist_version_from    => 'lib/WWW/Azantek/Diary.pm',
+    dist_abstract        => 'WWW::Azantek::Diary',
+    license              => 'perl',
+    recursive_test_files => 1,
+    create_readme        => 1,
+    configure_requires   => {},
+    create_makefile_pl   => 'traditional',
+    build_requires       => {
+        'Test::More'                => '0', 
+        'File::Slurp'               => '0',
+        'Test::Exception'           => '0',
+        'Test::MockObject::Extends' => '0',
+        'Test::Perl::Critic'        => '0',
+        'Test::Prereq::Build'       => '0',
+        'Env'                       => '0',
+    },
+    requires => {
+        'Module::Build'             => '0.30',
+        'Data::Dumper'           => '0',    #Debugging
+        'WWW::Mechanize'         => '0',
+        'WWW::Mechanize::Cached' => '0',
+        'Carp'                   => '0',
+    },
+    meta_merge => {
+        resources => {
+            homepage => 'http://www.azantek.com'
+			}
+}
+
+);
+
+
+
+$build2->create_build_script();
+
+
+
+my $build3 = Module::Build->new(
+    dist_author          => 'Prtik Dam <pdam@gmail.com>',
+    module_name          => 'WWW::Azantek::Diary',
+    dist_version_from    => 'lib/WWW/Azantek/Diary.pm',
+    dist_abstract        => 'WWW::Azantek::Diary',
+    license              => 'perl',
+    recursive_test_files => 1,
+    create_readme        => 1,
+    configure_requires   => {},
+    create_makefile_pl   => 'traditional',
+    build_requires       => {
+        'Test::More'                => '0', 
+        'File::Slurp'               => '0',
+        'Test::Exception'           => '0',
+        'Test::MockObject::Extends' => '0',
+        'Test::Perl::Critic'        => '0',
+        'Test::Prereq::Build'       => '0',
+        'Env'                       => '0',
+    },
+    requires => {
+        'Module::Build'             => '0.30',
+        'Data::Dumper'           => '0',    #Debugging
+        'WWW::Mechanize'         => '0',
+        'WWW::Mechanize::Cached' => '0',
+        'Carp'                   => '0',
+    },
+    meta_merge => {
+        resources => {
+            homepage => 'http://www.azantek.com',
+            bugtracker =>
+	}
+}
+);
+
+
+$build3->create_build_script();
+
+
+my $build4 = Module::Build->new(
+    dist_author          => 'Prtik Dam <pdam@gmail.com>',
+    module_name          => 'WWW::Azantek::Diary',
+    dist_version_from    => 'lib/WWW/Azantek/Diary.pm',
+    dist_abstract        => 'WWW::Azantek::Diary',
+    license              => 'perl',
+    recursive_test_files => 1,
+    create_readme        => 1,
+    configure_requires   => {},
+    create_makefile_pl   => 'traditional',
+    build_requires       => {
+        'Test::More'                => '0', 
+        'File::Slurp'               => '0',
+        'Test::Exception'           => '0',
+        'Test::MockObject::Extends' => '0',
+        'Test::Perl::Critic'        => '0',
+        'Test::Prereq::Build'       => '0',
+        'Env'                       => '0',
+    },
+    requires => {
+        'Module::Build'             => '0.30',
+        'Data::Dumper'           => '0',    #Debugging
+        'WWW::Mechanize'         => '0',
+        'WWW::Mechanize::Cached' => '0',
+        'Carp'                   => '0',
+    },
+    meta_merge => {
+        resources => {
+            homepage => 'http://www.azantek.com',
+            bugtracker =>
+	}
+}
+);
+
+
+
+
+
+
+
+
+my $build5 = Module::Build->new(
+    dist_author          => 'Prtik Dam <pdam@gmail.com>',
+    module_name          => 'WWW::Azantek::Hours',
+    dist_version_from    => 'lib/WWW/Azantek/Hours.pm',
+    dist_abstract        => 'WWW::Azantek::Hours',
+    license              => 'perl',
+    recursive_test_files => 1,
+    create_readme        => 1,
+    configure_requires   => {},
+    create_makefile_pl   => 'traditional',
+    build_requires       => {
+        'Test::More'                => '0', 
+        'File::Slurp'               => '0',
+        'Test::Exception'           => '0',
+        'Test::MockObject::Extends' => '0',
+        'Test::Perl::Critic'        => '0',
+        'Test::Prereq::Build'       => '0',
+        'Env'                       => '0',
+    },
+    requires => {
+        'Module::Build'             => '0.30',
+        'Data::Dumper'           => '0',    #Debugging
+        'WWW::Mechanize'         => '0',
+        'WWW::Mechanize::Cached' => '0',
+        'Carp'                   => '0',
+    },
+    meta_merge => {
+        resources => {
+            homepage => 'http://www.azantek.com',
+            bugtracker =>
+	}
+}
+);
+
+
+
+
+
+
+
+1;
+
---
+++ t/retrieve.t
@@ -0,0 +1,114 @@
+#!/usr/bin/perl -w
+
+# $Id: retrieve.t 6983 2010-02-20 20:14:51Z jonasbn $
+
+use strict;
+use warnings;
+use Test::More tests => 10;
+use Carp;
+use Test::MockObject::Extends;
+use File::Slurp qw(slurp);
+use WWW::Azantek;
+use Test::Exception;
+use Env qw(TEST_VERBOSE);
+
+my $mech = Test::MockObject::Extends->new('WWW::Mechanize');
+my $wd;
+
+$mech->mock(
+    'content',
+    sub {
+        my ( $mb, %params ) = @_;
+
+        my $content = slurp('t/testdata')
+            || croak "Unable to read file - $!";
+
+        return $content;
+    }
+);
+$mech->set_true('get', 'follow_link', 'submit_form');
+
+my $content;
+
+$wd = WWW::Azantek->new({
+	username => 'tester@tester.com',
+	password => 'topsecret',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+can_ok($wd, qw(retrieve));
+
+ok($content = $wd->retrieve());
+
+isa_ok($content, 'SCALAR');
+
+is($$content, 'test');
+
+$mech->set_series('get', 1, 1);
+$mech->set_series('submit_form', undef);
+
+$wd = WWW::Azantek->new({
+	username => 'tester@tester.com',
+	password => 'topsecret',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+dies_ok { $content = $wd->retrieve(); };
+
+$mech->set_series('get', undef, undef);
+$mech->set_series('submit_form', 1);
+
+$wd = WWW::Azantek->new({
+	username => 'tester@tester.com',
+	password => 'topsecret',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+dies_ok { $content = $wd->retrieve(); };
+
+$mech->set_series('get', 1, undef);
+
+$wd = WWW::Azantek->new({
+	username => 'tester@tester.com',
+	password => 'topsecret',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+dies_ok { $content = $wd->retrieve(); };
+
+$wd = WWW::Azantek->new({
+	password => 'topsecret',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+dies_ok { $content = $wd->retrieve(); };
+
+$wd = WWW::Azantek->new({
+	username => 'tester@tester.com',
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => $TEST_VERBOSE,
+    mech     => $mech,
+});
+
+dies_ok { $content = $wd->retrieve(); };
+
+$mech->set_true('get', 'follow_link', 'submit_form');
+
+$wd = WWW::Azantek->new({
+	url      => 'http://test.azantek.com/cgi-bin',
+    verbose  => 1,
+    mech     => $mech,
+});
+
+ok($content = $wd->retrieve());
+
